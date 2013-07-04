01ef1b2886543fe04754664575a80b9e82a59650
Bo Zhu <github@bozhu.me>
2013-07-03 22:24:50 -0400

    skip version check of gae sdk; added stdout.flush()

--- test/run-all-tests.py
+++ test/run-all-tests.py
@@ -18,6 +18,7 @@ except Exception as exp:
     print 'is not installed.'
     print 'Please install it and try again.'
     sys.exit(-1)
+sys.stdout.flush()
 
 
 server_process = None
@@ -26,13 +27,16 @@ server_process = None
 def start_server():
     global server_process
     print 'To start the server, and wait for 10 seconds to set up...'
-    server_process = subprocess.Popen(['dev_appserver.py', '..'])
+    sys.stdout.flush()
+    server_process = subprocess.Popen(
+        ['dev_appserver.py', '--skip_sdk_update_check=yes', '..'])
     time.sleep(10)
 
 
 def stop_server():
     time.sleep(1)
     print 'To stop the server...',
+    sys.stdout.flush()
     server_process.terminate()
     server_process.wait()
     print 'done.'
@@ -41,11 +45,13 @@ def stop_server():
 # http://goo.gl/xaBer
 def red_alert(text):
     print "\033[7;31m" + text + "\033[0m"
+    sys.stdout.flush()
 
 
 def run_all_tests():
     print
     print 'To run all test-*.js files...'
+    sys.stdout.flush()
     num_failed = 0
     num_passed = 0
     for file_name in os.listdir('.'):
@@ -53,6 +59,7 @@ def run_all_tests():
             command = ['phantomjs', file_name]
             print
             print ' '.join(command)
+            sys.stdout.flush()
             return_value = subprocess.call(command)
             time.sleep(2)  # sleep 2 seconds between tests
             if return_value != 0:
@@ -61,6 +68,7 @@ def run_all_tests():
             else:
                 num_passed += 1
                 print file_name + ' passed.'
+                sys.stdout.flush()
     print
     if num_failed > 0:
         red_alert('Final results: ' + str(num_failed) + ' TESTS FAILED'
@@ -68,6 +76,7 @@ def run_all_tests():
     else:
         print 'All %d tests passed.' % (num_passed + num_failed)
     print
+    sys.stdout.flush()
     return num_failed
 
 
